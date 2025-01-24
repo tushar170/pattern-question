ac84243a1b0f03d13787304a4e590e46b3df8e74
Tushar Agarwal <131223576+tushar170@users.noreply.github.com>
2025-01-24 17:21:52 +0530

    Update pattern1.java

--- pattern1.java
+++ pattern1.java
@@ -1,6 +1,3 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-
 class Main {
     public static void main(String[] args) {
         for(int i=1;i<=5;i++){
