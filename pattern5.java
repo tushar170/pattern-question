dedf8ef187b55722ad74f0382ba67999da8fd2a0
Tushar Agarwal <131223576+tushar170@users.noreply.github.com>
2025-01-24 18:02:54 +0530

    Create pattern5.java

---
+++ pattern5.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int s =0;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                s++;
+                System.out.print((char)(s+64)+" ");
+            }
+        System.out.println();
+        }
+    }
+}
