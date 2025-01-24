c5f6981a8dbd5356021f8fc1e5bb8a6dd7cb02d8
Tushar Agarwal <131223576+tushar170@users.noreply.github.com>
2025-01-24 17:48:16 +0530

    Create patten3.java

---
+++ patten3.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        int s =0;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                s++;
+                System.out.print(s+" ");
+            }
+        System.out.println();
+    }
+        
+    }
+}
