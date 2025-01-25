d5659c6f10b9fe1c14faa474683cbdaac51f5fc8
Tushar Agarwal <131223576+tushar170@users.noreply.github.com>
2025-01-25 10:48:46 +0530

    Create pattern9.java

---
+++ pattern9.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        int s =0;
+        for(int i=0;i<5;i++){
+            for(int j=1;j<=5-i;j++)
+            System.out.print(j);
+           System.out.println();
+        }
+    }
+}
+/*
+12345
+1234
+123
+12
+1
+*/
