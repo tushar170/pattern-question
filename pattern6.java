58ffb2e1b4488c1249fe39acc9c098ecf6623456
Tushar Agarwal <131223576+tushar170@users.noreply.github.com>
2025-01-25 09:57:05 +0530

    pattern6.java

---
+++ pattern6.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        int s =0;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(i==1||i==5||j==1||j==5)
+                System.out.print("*");
+                else
+                System.out.print(" ");
+            }
+        System.out.println();
+        }
+    }
+}
