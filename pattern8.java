0def63af22df913cd330e69d0c46ec14c89ab20a
Tushar Agarwal <131223576+tushar170@users.noreply.github.com>
2025-01-25 10:23:33 +0530

    Create pattern8.java

---
+++ pattern8.java
@@ -0,0 +1,19 @@
+class Main {
+    public static void main(String[] args) {
+        int s =0;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5-i;j++)
+            System.out.print(" ");
+            for(int j =1;j<=i;j++) 
+            System.out.print("*");
+        System.out.println();
+        }
+    }
+}
+/*
+    *
+   **
+  ***
+ ****
+*****
+*/
