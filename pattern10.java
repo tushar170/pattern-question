d220ea4921fafff9f78434f13851cc384f7a4626
Tushar Agarwal <131223576+tushar170@users.noreply.github.com>
2025-01-25 10:51:56 +0530

    Create pattern10.java

---
+++ pattern10.java
@@ -0,0 +1,20 @@
+class Main {
+    public static void main(String[] args) {
+        int s =0;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5-i;j++)
+            System.out.print(" ");
+            for(int j =1;j<=2*i-1;j++) 
+            System.out.print("*");
+        System.out.println();
+        }
+    }
+}
+/*
+    *
+   ***
+  *****
+ *******
+*********
+
+*/
